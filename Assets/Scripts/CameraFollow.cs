dd52686d9c1b0c1803216fa3d764b5c2d9b08956
Nick <flyman14@live.ca>
2017-04-01 19:45:43 -0700

    Initial commit. Started on player and camera follow controllers.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Causes the camera to follow around a target. 
+/// Target can move within the camera area a specified amount before starting to follow.
+/// </summary>
+public class CameraFollow : MonoBehaviour {
+    //Transform the camera is set to follow around.
+    public Transform target;
+    //Maximum distances the camera can travel in the x direction. 
+    public float xBoundary;
+    //Maximum distances the camera can travel in the z direction.
+    public float zBoundary;
+    // Maximum distance the target can move within the camera area without moving the camera in the x direction.
+    public float xBounce;
+    // Maximum distance the target can move within the camera area without moving the camera in the z direction.
+    public float yBounce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the player object.
+/// </summary>
+public class PlayerController : MonoBehaviour {
+    //Forward thrust speed.
+    public float thrustSpeed;
+    //Deceleration speed.
+    public float stopSpeed;
+    //Speed to turn the ship.
+    public float turnSpeed;
+    //Rate at which weapons can fire.
+    public float fireRate;
+
+    //The object spawned upon firing.
+    public GameObject shot;
+    //Empty GameObject used for the spawn location of shots.
+    public GameObject shotSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
