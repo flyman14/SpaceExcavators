42bc965c962172a35d9e4ef9d9e988c5ae780393
Nick <flyman14@live.ca>
2017-04-05 20:06:36 -0700

    Shop UI design, beginning shop code.

---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour {
+    bool canOpenShop = false;
+    bool keyReleaseShop = true;
+
+    public static Canvas shopCanvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canOpenShop = true;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canOpenShop = false;
+            if (shopCanvas.enabled)
+            {
+                shopCanvas.enabled = false;
+            }
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (canOpenShop)
+        {
+            if (Input.GetButton("ShopToggle"))
+            {
+                if (keyReleaseShop)
+                {
+                    shopCanvas.enabled = !shopCanvas.enabled;
+                    keyReleaseShop = false;
+                }
+
+            }
+            else
+            {
+                keyReleaseShop = true;
+            }
+        }
+    }
+
+}
