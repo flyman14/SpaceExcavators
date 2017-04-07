c29919335d972f383cddb9c954bfa0bfa9b6ee57
Nick <flyman14@live.ca>
2017-04-07 15:36:06 -0700

    Scrolling BG and particle starfield.

---
+++ Assets/Scripts/UI/BGScroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+    public float scrollFactor;
+    public float tileSizeX;
+    public float tileSizeZ;
+    public Transform rootTransform;
+
+    //Vector3 startPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float newPosX;
+        float newPosY;
+
+        newPosX = Mathf.Repeat(rootTransform.position.x * scrollFactor, tileSizeX);
+        newPosY = Mathf.Repeat(rootTransform.position.z * scrollFactor, tileSizeZ);
+
+        //Debug.Log("newPosY: " + newPosY);
+
+        gameObject.transform.localPosition = new Vector3(-newPosX, -newPosY, 40f);
+    }
+}
